8eb1be5c87fdb31176c4368d5770c299478fbb20
“[SUMITGAIKWAD90]” <[om1929390@gmail.com]>
2026-02-03 12:47:01 +0530

    Updated

--- config-template.js
+++ config-template.js
@@ -1,6 +1,3 @@
-// config-template.js
-// Copy this file to config.js and add your EmailJS credentials
-
 const EMAILJS_CONFIG = {
   PUBLIC_KEY: "your_public_key_here",
   SERVICE_ID: "your_service_id_here",
